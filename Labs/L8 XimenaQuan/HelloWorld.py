6705539b611301c858a57deccb5bce061e696dff
XimenaQuan <xmquan@correo.url.edu.gt>
2024-05-15 09:38:17 -0600

    act

--- Labs/L8 XimenaQuan/HelloWorld.py
+++
@@ -1,8 +0,0 @@
-saludo = 'Hola'
-nombre = input('ingrese su nombre \n')
-print(saludo, nombre)
-
-numero = int(input('ingrese un numero \n'))
-numero2 = int(input('ingrese otro número \n'))
-print('la suma es',numero + numero2)
-
